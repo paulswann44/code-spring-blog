696b6264895a2433dbb3b976b97dce5a1d634fbd
paulswann44 <99698080+paulswann44@users.noreply.github.com>
2023-02-20 15:53:34 -0600

    ...

--- src/main/java/com/codeup/codeupspringblog/controllers/CodeupSpringBlogApplication.java
+++ src/main/java/com/codeup/codeupspringblog/controllers/CodeupSpringBlogApplication.java
@@ -1,4 +1,4 @@
-package com.codeup.codeupspringblog;
+package com.codeup.codeupspringblog.controllers;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
